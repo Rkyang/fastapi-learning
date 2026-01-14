2e22e4e91245f481db4f658c94c3d0829bc02568
rkyang <rkyang@outlook.com>
2026-01-14 10:45:39 +0800

    06路径参数-Path类型注解

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+@app.get("/news/byName/{news_name}")
+async def news_by_name(news_name: str = Path(..., min_length=2, max_length=10, description='按分类名称获取，名称长度2-10')):
+    return {"news_name": news_name}
+
+@app.get("/news/byId/{news_id}")
+async def news_by_id(news_id: int = Path(..., gt=0, lt=101, description='按分类id获取，id范围1-100')):
+    return {"新闻分类id": news_id}
 
 @app.get("/user/{user_id}")
 async def get_user(user_id: str):
