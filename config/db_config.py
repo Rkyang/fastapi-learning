68c987786ba569592c45c66751a2f15c0d6d4417
rkyang <rkyang@outlook.com>
2026-01-19 16:39:56 +0800

    33数据库orm配置

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 # .env文件路径
 env_path = Path(__file__).resolve().parent.parent / '.env'
@@ -13,5 +15,35 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
+# 数据库连接url
 DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+# 创建异步引擎
+engine = create_async_engine(
+    DATABASE_URL,
+    # 输出sql日志
+    echo=True,
+    # 连接池持久连接数
+    pool_size=10,
+    # 额外连接数
+    max_overflow=20,
+)
+
+# 创建异步会话工厂
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+# 获取数据库会话
+async def get_db():
+    async with SessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
