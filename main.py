018abdf51336037d9c413cda617b05b4ef6914ce
rkyang <rkyang@outlook.com>
2026-01-23 17:36:18 +0800

    36跨域问题

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from routes import news_category
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 允许跨域
+app.add_middleware(
+    CORSMiddleware,
+    # 允许源
+    allow_origins=["*"],
+    # 允许携带token
+    allow_credentials=True,
+    # 允许请求方法类型
+    allow_methods=["*"],
+    # 允许请求头
+    allow_headers=["*"]
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
