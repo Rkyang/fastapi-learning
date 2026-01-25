31252da9852af575aa88b3705437291c5835291d
Rkyang <rkyang@outlook.com>
2026-01-25 10:39:20 +0800

    46全局异常处理器

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from routes import news, user
 from fastapi.middleware.cors import CORSMiddleware
 
+from utils.exception_handles import register_exception_handlers
+
 app = FastAPI()
+
+# 注册全局异常处理
+register_exception_handlers(app)
 
 # 允许跨域
 app.add_middleware(
